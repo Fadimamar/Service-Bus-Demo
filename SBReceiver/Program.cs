e30fe95fcaaf70562d9de8842369a1014d8736b1
Fadi Mamar <fadimamar@hotmail.com>
2022-01-02 11:31:29 -0800

    commit three projects

---
+++ SBReceiver/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using SBShared.Models;
+using Microsoft.Azure.ServiceBus;
+using System.Text;
+using System.Text.Json;
+
+
+namespace SBReceiver
+{
+    class Program 
+    { 
+        const string connectionString= "Service Bus Connection String";
+        const string Queuename="Queue Name in your Service Bus";
+        static IQueueClient queueClient;
+        static async Task Main(string[] args)
+        {
+            queueClient=new QueueClient(connectionString, Queuename);
+            var messageHandlerOptions=new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                AutoComplete=false, //done auto complete. Message will be marked complete after peek and capturing of the message body for further processing
+                MaxConcurrentCalls=1 //process one meassge at a time
+            };
+            //start listening to queue by registering a message handler with above options.
+            queueClient.RegisterMessageHandler(ProcessMessageAsync, messageHandlerOptions);
+            
+            Console.ReadLine(); //keep listening until enter is pressed.
+            await queueClient.CloseAsync(); //close connection with Azure Service Bus
+
+         }
+        static async Task ProcessMessageAsync(Message message,CancellationToken token )
+        {
+            var jsonString=Encoding.UTF8.GetString(message.Body); //get json string
+            PersonModel person = JsonSerializer.Deserialize<PersonModel>(jsonString); //Convert to person model
+            Console.WriteLine($"Person Received:{person.FirstName} {person.LastName}"); // This is where we do the work like sending email to person, update database, etc.
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);// mark message complete withing the 30 seconds locktime or the lock time configured on Azure for the service bus queue. 
+        }
+        static  Task ExceptionReceivedHandler(ExceptionReceivedEventArgs arg)
+        {
+            Console.WriteLine($"Message Recevied Exception: {arg.Exception}");
+            return Task.CompletedTask; //since a task is expected in return.
+        }
+    }
+}
---
+++ SBSender/Services/QueueService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.ServiceBus;
+using System.Text.Json;
+using System.Text;
+
+namespace SBSender.Services
+{
+    public class QueueService : IQueueService
+    {
+        private readonly IConfiguration _configuration;
+        public QueueService (IConfiguration configuration)
+        {
+            _configuration = configuration; //inject configuration
+        }
+        public async Task SendMessageAsync<T> (T ServiceBusMessage, string queueName)
+        {
+            var queueClient = new QueueClient(_configuration.GetConnectionString("AzureServiceBus"), queueName);
+            string messageBody = JsonSerializer.Serialize(ServiceBusMessage); //Convert to json string
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody)); //convert to byte array
+            await queueClient.SendAsync(message);
+
+        }
+    }
+}
